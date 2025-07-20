7c173340c8f40c5563d315b4caf997a369fd19cf
youtusave <youtusave@gmail.com>
2025-07-20 03:07:07 -0700

    feat: Update ide/1838bb01-c749-4d0a-9baf-ac3e3934b4c2/script.js

--- ide/1838bb01-c749-4d0a-9baf-ac3e3934b4c2/script.js
+++ ide/1838bb01-c749-4d0a-9baf-ac3e3934b4c2/script.js
@@ -1,3 +1,8 @@
-function sayHello() {
-  alert('The compiler works!');
+document.getElementById('changeColorBtn').addEventListener('click', function () {
+  document.body.style.backgroundColor = getRandomColor();
+});
+
+function getRandomColor() {
+  const colors = ['#FFADAD', '#FFD6A5', '#FDFFB6', '#CAFFBF', '#A0C4FF', '#BDB2FF', '#FFC6FF'];
+  return colors[Math.floor(Math.random() * colors.length)];
 }
